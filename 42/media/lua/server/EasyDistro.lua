2501419d44d36c7e47822cd19fa0c08a64174785
Doug <doug.stalker@gmail.com>
2025-06-01 19:31:07 +1000

    added vehicle loot

--- 42/media/lua/server/EasyDistro.lua
+++ 42/media/lua/server/EasyDistro.lua
@@ -1,10 +1,14 @@
 require "Items/ProceduralDistributions"
---require "Vehicles/VehicleDistribution"
+require "Vehicles/VehicleDistributions"
 
 
 EasyDistro =  {}
 EasyDistro.Debug = true
 EasyDistro.VerboseDebug = false
+
+--EasyDistro.LootTableBase  = ProceduralDistributions.list
+--EasyDistro.LootTableBaseV = VehicleDistributions
+EasyDistro.LootTables = {mainloot = ProceduralDistributions.list, vehicleloot = VehicleDistributions}
 
 
 function EasyDistro.DebugSay(message)
@@ -31,9 +35,10 @@ function EasyDistro.EnableVerbose(bool)
 end
 
 -- ----------------------------------------------------
--- Find all the loot table entries for an item, returns them as a table of {lootTable=, probabilty=}
+-- Find all the loot table entries for an item, returns them as a table of {lootSubTable=, probabilty=}
+-- lootTable is a parameter so it can be passed teh main loot table or teh vehicle loot table
 
-function EasyDistro.FindItemLootValues(refItem)
+function EasyDistro.FindItemLootValues(refItem, lootTable)
     local retValues={}
     local found=0
     --refItem might be a short or full item name, and loot tables use both so:
@@ -44,14 +49,16 @@ function EasyDistro.FindItemLootValues(refItem)
     end
     local fullName=script:getFullName()
     local shortName=script:getName()
-    for k,v in pairs(ProceduralDistributions.list) do
+    for k,v in pairs(lootTable) do
         --k -> tablename, v.items -> the list of stuff in the table
-        for j,w in pairs(v.items) do
-            if w==fullName or w==shortName then
-                EasyDistro.DebugSayVerbose(tostring(k).." "..tostring(w).." "..tostring(v.items[j+1]))
-                table.insert(retValues, {lootTable=k, probabilty=(v.items[j+1])})
-                --retValues[k]=v.items[j+1]
-                found=found+1
+        if v.items then --not all entries in the vehicle table have an items table
+            for j,w in pairs(v.items) do
+                if w==fullName or w==shortName then
+                    EasyDistro.DebugSayVerbose(tostring(k).." "..tostring(w).." "..tostring(v.items[j+1]))
+                    table.insert(retValues, {lootSubTable=k, probabilty=(v.items[j+1])})
+                    --retValues[k]=v.items[j+1]
+                    found=found+1
+                end
             end
         end
     end
@@ -64,21 +71,23 @@ end
 
 function EasyDistro.AddItem(newItem, refItem, lootMult)
     if lootMult == nil then lootMult=1.0 end --default if not specified
-    if type(newItem) ~= "string" or type(refItem) ~= "string" or type(lootMult) ~= "number"  then
-        EasyDistro.ErrorSay("newItem and refItem must be strings and (if used) lootMult must be a number.")
+    if type(newItem) ~= "string" or type(refItem) ~= "string" or type(lootMult) ~= "number" or lootMult <= 0 then
+        EasyDistro.ErrorSay("newItem and refItem must be strings and (if used) lootMult must be a number greater than zero.")
         EasyDistro.ErrorSay("newItem: "..type(newItem).." refItem: "..type(refItem).." lootMult: "..type(lootMult))
         return
     end
-    local refValues = EasyDistro.FindItemLootValues(refItem)
-    local added = 0
-    for k,v in pairs(refValues) do
-        EasyDistro.DebugSayVerbose("Adding "..newItem.." to table "..v.lootTable.." value: "..string.format("%.3f",v.probabilty * lootMult))
-        table.insert(ProceduralDistributions["list"][v.lootTable].items, newItem);
-        table.insert(ProceduralDistributions["list"][v.lootTable].items, v.probabilty * lootMult);
-        added=added+1
+    --for main and vehicle tables.
+    for k,lootTable in pairs(EasyDistro.LootTables) do
+        local refValues = EasyDistro.FindItemLootValues(refItem,lootTable)
+        local added = 0
+        for k2,v in pairs(refValues) do
+            EasyDistro.DebugSayVerbose("Adding "..newItem.." to table ["..k.."]"..v.lootSubTable.." value: "..string.format("%.3f",v.probabilty * lootMult))
+            table.insert(lootTable[v.lootSubTable].items, newItem);
+            table.insert(lootTable[v.lootSubTable].items, v.probabilty * lootMult);
+            added=added+1
+        end
+        EasyDistro.DebugSay("Added "..added.." loot entries to "..tostring(k).." for "..newItem.." based on "..refItem.." with a "..string.format("%.3f",lootMult).." modifier")
     end
-    EasyDistro.DebugSay("Added "..added.." loot entries for "..newItem.." based on "..refItem.." with a "..string.format("%.3f",lootMult).." modifier")
-    
 end
 
 -- ----------------------------------------------------
@@ -88,7 +97,18 @@ function EasyDistro.UpdateItemPicker()
     ItemPickerJava:Parse() -- Updates the java loot generation with the new Values from ProceduralDistributions
 end    
 
---EasyDistro.Add("Base.NepPR57","Base.ArmorSchematic",1)
---EasyDistro.Add("Base.NepPR57","Base.Pistol",1)
+
+-- TODO:
+
+--Remove item from matching locations
+--function EasyDistro.RemoveFromLocation(item, location)
+--end
+
+--Remove item from matching locations
+--function EasyDistro.RemoveFromAll(item)
+--end
+
+
+
 
 return EasyDistro
